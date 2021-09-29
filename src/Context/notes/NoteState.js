29e92c94db28e0d2317c3d08f78e6ca32cb4408e
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-29 20:20:38 +0530

    Adding AddNote component to iNotebook
    Client Side ready , need to work on server side

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -78,9 +78,30 @@ const NoteState = (props) => {
   ]
 
   const [notes, setNotes] = useState(notesIntial)
-    
+
+  // Add a Note
+  const addNote = (title,description,tag) => {
+    // TODO API CALL
+    const note = {"_id": "6148596e1940ea966656f370",
+    "user": "61483de0d1306251693050ea",
+    "title":title,
+    "description": description,
+    "tag":tag,
+    "date": "2021-09-20T09:50:38.843Z",
+    "__v": 0}
+    setNotes(notes.concat(note))
+  }
+  // Delete a Note
+  const deleteNote = (id) => {
+
+  }
+  
+  // Edit a Note
+  const editNote = (id) => {
+
+  }
 return (
-   <NoteContext.Provider value={{notes,setNotes}}>
+   <NoteContext.Provider value={{notes,setNotes,addNote,deleteNote,editNote}}>
     {props.children}
    </NoteContext.Provider>
  )
