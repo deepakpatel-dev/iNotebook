e5eb4aaac55e020f7085a3348ca158afc9341761
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-30 11:34:56 +0530

    Adding 'Modal' to update Note

--- src/Components/AddNote.js
+++ src/Components/AddNote.js
@@ -28,9 +28,9 @@ const AddNote = () => {
                     <label htmlFor="description" className="form-label">Description</label>
                     <input type="text" className="form-control" id="description" name="description" onChange={handleChange}/>
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <div className="mb-3">
+                    <label htmlFor="tag" className="form-label">Tag</label>
+                    <input type="text" className="form-control" id="tag" name="tag" onChange={handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
                 </form>
--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -47,12 +47,21 @@ const NoteState = (props) => {
     setNotes(notes.concat(note));
   };
   // Delete a Note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
     // TODO: API CALL
-    console.log("deleteing note", id);
-    const newNotes = notes.filter((note) => {
-      return note._id !== id;
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0ODNkZTBkMTMwNjI1MTY5MzA1MGVhIn0sImlhdCI6MTYzMjEyNDM4NH0.4QyDbCdNOjCcqzV157Bmv49yfqHRdmZacEsUTxNK0RY",
+      },
+      
     });
+
+    const json = response.json();
+    console.log("deleteing note", id);
+    const newNotes = notes.filter((note) => { return note._id !== id; });
     setNotes(newNotes);
   };
 
