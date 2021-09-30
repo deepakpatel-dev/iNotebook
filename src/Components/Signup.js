d1a2decdcd7378bfb49d34131dc37bce4304c783
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-30 20:06:30 +0530

    Adding SignUp Component to iNotebook

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useHistory } from 'react-router-dom'
 
-const Signup = () => {
+
+const Signup = (props) => {
+    const [credentials, setCredentials] = useState({name:"", email: "", password: "", cpassword:""}) 
+    
+    let history = useHistory();
+
+    const handleSubmit = async (e) => {
+        const {name,email,password} = credentials;
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name,email,password})
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success){
+            // Save the auth token and redirect
+            localStorage.setItem('token', json.authtoken); 
+            history.push("/");
+
+        }
+        else{
+            alert("Invalid credentials");
+        }
+    }
+
+    const onChange = (e)=>{
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
+ 
     return (
         <div>
-            
+            <form  onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" onChange={onChange} id="name" name="name" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text"></div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" onChange={onChange} name="password" id="password" minLength={5} required/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" onChange={onChange} name="cpassword" id="cpassword" minLength={5} required/>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
