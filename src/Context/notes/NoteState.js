1d88d10342442d7a90de3b15399a0974650bd1b5
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-30 14:11:09 +0530

    Updating Notes on edit in the UI using React
    Need to FIX : when you add first time without loading you can delete or edit from UI but wont reflect in Database.

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -34,6 +34,8 @@ const NoteState = (props) => {
         body: JSON.stringify({title,description,tag}),
       });
     
+      const json = await response.json();
+      console.log(json)
 
     const note = {
       _id: "6148596e1940ea966656f370",
@@ -64,12 +66,12 @@ const NoteState = (props) => {
     const newNotes = notes.filter((note) => { return note._id !== id; });
     setNotes(newNotes);
   };
-
+  
   // Edit a Note
   const editNote = async (id, title, description, tag) => {
     // API CALL
       const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-        method: "POST",
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
           "auth-token":
@@ -77,19 +79,21 @@ const NoteState = (props) => {
         },
         body: JSON.stringify({title,description,tag}),
       });
-      const json = response.json();
+      const json = await response.json();
+    console.log(json)
     
-
+        let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   };
   return (
     <NoteContext.Provider
